ad645da50102832922bb7ff61eb7a5d6a5e62822
wwj-glb <goodMorning-glb@wenju.com>
2022-05-07 17:56:56 +0800

    添加连接数据库的lib包

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -1,17 +1,15 @@
 package servlet;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
 @WebServlet(value = "/loginServlet")
 public class LoginServlet extends HttpServlet {
 
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("aaaa");
+//        response.getWriter().println("aaaa");
+
     }
 }
